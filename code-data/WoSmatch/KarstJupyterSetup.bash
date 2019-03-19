0e212392af1cff22be85d0d94357150cc22b38e3
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-03-19 00:11:35 -0400

    Update KarstJupyterSetup.bash

--- code-data/WoSmatch/KarstJupyterSetup.bash
+++ code-data/WoSmatch/KarstJupyterSetup.bash
@@ -3,7 +3,7 @@ pip install --user Toree
 export PATH=~/.local/bin:$PATH
 jupyter toree install --spark_home=$SPARK_HOME
 jupyter kernelspec list
-jupyter toree install --replace --spark_home=$SPARK_HOME --kernel_name="Spark" --spark_opts="--packages com.databricks:spark-xml_2.11:0.5.0 --master spark://c240.karst.uits.iu.edu:7077 --driver-memory 32G --executor-memory 5G --executor-cores 3 --conf spark.driver.maxResultSize=8g" --user
+jupyter toree install --replace --spark_home=$SPARK_HOME --kernel_name="Spark" --spark_opts="--packages com.databricks:spark-xml_2.11:0.5.0 --master spark://c240.karst.uits.iu.edu:7077 --driver-memory 8G --executor-memory 10G --executor-cores 5 --conf spark.driver.maxResultSize=8g" --user
 jupyter notebook --no-browser --port=8000
 -----------To check | kill ports, use
 lsof -ti:8000 | xargs kill -9
