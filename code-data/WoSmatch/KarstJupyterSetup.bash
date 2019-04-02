c4b9a5324245f5ff21b6dfa9ad5a5e9d85880422
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-04-01 20:00:57 -0400

    Update KarstJupyterSetup.bash

--- code-data/WoSmatch/KarstJupyterSetup.bash
+++ code-data/WoSmatch/KarstJupyterSetup.bash
@@ -1,7 +1,13 @@
 ssh yan30@karst.uits.iu.edu
+
+module load java #load sun jdk10 for hadoop
+module load hadoop
+module load spark
+module unload java #use openjdk instead for scala interactive
+
 pip install --user Toree
 export PATH=~/.local/bin:$PATH
-jupyter toree install --spark_home=$SPARK_HOME
+jupyter toree install --spark_home=$SPARK_HOME --user
 jupyter kernelspec list
 jupyter toree install --replace --spark_home=$SPARK_HOME --kernel_name="Spark" --spark_opts="--packages com.databricks:spark-xml_2.11:0.5.0 --master spark://c240.karst.uits.iu.edu:7077 --driver-memory 8G --executor-memory 10G --executor-cores 5 --conf spark.driver.maxResultSize=8g" --user
 jupyter notebook --no-browser --port=8000
