a6b634b6d6bb6b816770c88fe03760f40091e4cd
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-04-16 11:07:48 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,4 @@
 install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages('leaflet')
 install.packages("widyr")
 install.packages("igraph")
 install.packages("gender")
