b03affb54c6a478def26c4b9652f4b2c70030905
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-06-10 13:24:38 -0400

    Update KarstJupyterSetup.bash

--- code-data/WoSmatch/KarstJupyterSetup.bash
+++ code-data/WoSmatch/KarstJupyterSetup.bash
@@ -8,7 +8,8 @@ module load hadoop
 module load spark
 module unload java #use openjdk instead for scala interactive
 
-module swap python/3.6.3
+module swap python/3.6.8
+export PATH=$PATH:~/.local/bin
 pip install jupyterlab --user
 jupyter serverextension enable --py jupyterlab
 
