b8d23e607c13462c1697c9cd62d8a6c6f7946927
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-07-31 12:32:32 -0400

    Update KarstJupyterSetup.bash

--- code-data/WoSmatch/KarstJupyterSetup.bash
+++ code-data/WoSmatch/KarstJupyterSetup.bash
@@ -8,6 +8,7 @@ module load hadoop
 module load spark
 module unload java #use openjdk instead for scala interactive
 
+cd /N/dc2/projects/IUNI_MSAcademic/
 module swap python/3.6.8
 export PATH=$PATH:~/.local/bin
 pip install jupyterlab --user
@@ -17,7 +18,7 @@ pip install --user Toree
 export PATH=~/.local/bin:$PATH
 jupyter toree install --spark_home=$SPARK_HOME --user
 jupyter kernelspec list
-jupyter toree install --replace --spark_home=$SPARK_HOME --kernel_name="Spark" --spark_opts="--packages com.databricks:spark-xml_2.11:0.5.0;org.zouzias:spark-lucenerdd_2.11:0.3.7 --master spark://c240.karst.uits.iu.edu:7077 --driver-memory 8G --executor-memory 15G --executor-cores 4 --conf spark.driver.maxResultSize=8g" --user
+jupyter toree install --replace --spark_home=$SPARK_HOME --kernel_name="Spark" --spark_opts="--packages com.databricks:spark-xml_2.11:0.5.0 --master spark://c240.karst.uits.iu.edu:7077 --driver-memory 8G --executor-memory 15G --executor-cores 4 --conf spark.driver.maxResultSize=8g" --user
 jupyter notebook --no-browser --port=8000
 -----------To check | kill ports, use
 lsof -ti:8000 | xargs kill -9
